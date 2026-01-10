8292b3b1b63c5ad4a90947e2b4eaf39a37bc122d
e4ercut <e4ercut@gmail.com>
2026-01-10 08:41:59 -0700

    redid api files

--- api/ping/index.js
+++ api/ping/index.js
@@ -1,8 +1,11 @@
 module.exports = async function (context, req) {
-  context.log("Ping endpoint hit");
+  context.log("Ping function hit");
 
   context.res = {
     status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: {
       message: "pong",
       time: new Date().toISOString()
