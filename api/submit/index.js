298159322785111f8942c0e4a05b168ee9edf3ae
e4ercut <110433623+e4ercut@users.noreply.github.com>
2026-01-11 19:01:59 -0700

    Uncomment code for handling submissions

--- api/submit/index.js
+++ api/submit/index.js
@@ -2,40 +2,40 @@
 
 module.exports = async function (context, req) {
   try {
-    //const { name, message } = req.body || {};
+    const { name, message } = req.body || {};
 
-    // if (!name || !message) {
-    //   context.res = { status: 400, body: { error: "Missing fields" } };
-    //   return;
-    // }
+    if (!name || !message) {
+      context.res = { status: 400, body: { error: "Missing fields" } };
+      return;
+    }
 
-    // const auth = new google.auth.GoogleAuth({
-    //   credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
-    //   scopes: ["https://www.googleapis.com/auth/spreadsheets"]
-    // });
+    const auth = new google.auth.GoogleAuth({
+      credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+    });
 
-    //const sheets = google.sheets({ version: "v4", auth });
+    const sheets = google.sheets({ version: "v4", auth });
 
-    //const spreadsheetId = "1t-BZ6sAla_LSoK9oQhjNTqq0U8VqOGjtB4d6IgRTI7g";
-    //const range = "Sheet1!A:C";
+    const spreadsheetId = "1t-BZ6sAla_LSoK9oQhjNTqq0U8VqOGjtB4d6IgRTI7g";
+    const range = "Sheet1!A:C";
 
-    // await sheets.spreadsheets.values.append({
-    //   spreadsheetId,
-    //   range,
-    //   valueInputOption: "RAW",
-    //   requestBody: {
-    //     values: [[
-    //       new Date().toISOString(),
-    //       name,
-    //       message
-    //     ]]
-    //   }
-    // });
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range,
+      valueInputOption: "RAW",
+      requestBody: {
+        values: [[
+          new Date().toISOString(),
+          name,
+          message
+        ]]
+      }
+    });
 
-    // context.res = {
-    //   status: 200,
-    //   body: { success: true }
-    // };
+    context.res = {
+      status: 200,
+      body: { success: true }
+    };
   } catch (err) {
     context.log(err);
     context.res = {
