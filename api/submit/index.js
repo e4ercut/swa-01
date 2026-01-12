b6c5debb850ae68a0d0a29575fca7cd842ab1e32
e4ercut <e4ercut@gmail.com>
2026-01-11 19:00:41 -0700

    changes to previous

--- api/submit/index.js
+++ api/submit/index.js
@@ -1,45 +1,45 @@
-const { google } = require("googleapis");
+//const { google } = require("googleapis");
 
 module.exports = async function (context, req) {
   try {
-    const { name, message } = req.body || {};
+    //const { name, message } = req.body || {};
 
-    if (!name || !message) {
-      context.res = { status: 400, body: { error: "Missing fields" } };
-      return;
-    }
+    // if (!name || !message) {
+    //   context.res = { status: 400, body: { error: "Missing fields" } };
+    //   return;
+    // }
 
-    const auth = new google.auth.GoogleAuth({
-      credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
-      scopes: ["https://www.googleapis.com/auth/spreadsheets"]
-    });
+    // const auth = new google.auth.GoogleAuth({
+    //   credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
+    //   scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+    // });
 
-    const sheets = google.sheets({ version: "v4", auth });
+    //const sheets = google.sheets({ version: "v4", auth });
 
-    const spreadsheetId = "https://docs.google.com/spreadsheets/d/1t-BZ6sAla_LSoK9oQhjNTqq0U8VqOGjtB4d6IgRTI7g/edit";
-    const range = "Sheet1!A:C";
+    //const spreadsheetId = "1t-BZ6sAla_LSoK9oQhjNTqq0U8VqOGjtB4d6IgRTI7g";
+    //const range = "Sheet1!A:C";
 
-    await sheets.spreadsheets.values.append({
-      spreadsheetId,
-      range,
-      valueInputOption: "RAW",
-      requestBody: {
-        values: [[
-          new Date().toISOString(),
-          name,
-          message
-        ]]
-      }
-    });
+    // await sheets.spreadsheets.values.append({
+    //   spreadsheetId,
+    //   range,
+    //   valueInputOption: "RAW",
+    //   requestBody: {
+    //     values: [[
+    //       new Date().toISOString(),
+    //       name,
+    //       message
+    //     ]]
+    //   }
+    // });
 
-    context.res = {
-      status: 200,
-      body: { success: true }
-    };
+    // context.res = {
+    //   status: 200,
+    //   body: { success: true }
+    // };
   } catch (err) {
     context.log(err);
     context.res = {
-      status: 500,
+      status: 505,
       body: { error: "Failed to write to Google Sheet" }
     };
   }
